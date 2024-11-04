9ed6a1cfa9583c5f7a4d6c4917047e5e38dbc5ac
acolalc878-tech <alejandrocolchero1@gmail.com>
2024-11-04 16:57:58 +0100

    Actualización del código:
    - Implementación de carpetas con la imagen del personaje
    - Cambios en el archivo Principal
    
    Día Lunes - 04/11/2024

--- Principal.py
+++ Principal.py
@@ -2,10 +2,9 @@
 
 import pygame
 import random
+
 from Personaje import Personaje
 import Constantes
-
-jugador = Personaje(50,50)
 
 # Inicializamos pygame
 pygame.init()
@@ -13,8 +12,17 @@ pygame.init()
 # Creamos la ventana
 ventana = pygame.display.set_mode((Constantes.ANCHO_VENTANA, Constantes.ALTO_VENTANA))
 
+# Actualizamos
+pygame.display.update()
+
 # Cambiamos el titulo de la ventana del juego
 pygame.display.set_caption("Videojuego por turnos")
+
+imagen_jugador = pygame.image.load("assets//images//character//player//player-run1.png ")
+
+# Creamos una instancia del jugador
+jugador = Personaje(50,50, imagen_jugador)
+
 
 # Definimos variables de movimiento del jugador
 mover_arriba = False
@@ -22,29 +30,33 @@ mover_abajo = False
 mover_derecha = False
 mover_izquierda = False
 
+# Controlamos los Frames por segundo
+reloj = pygame.time.Clock()
 
 # Bucle del juego
 run = True
 while run:
+
+    # Va a ir a 60 fps
+    reloj.tick(Constantes.FPS)
+
+    # Le asignamos color al fondo de la ventana
+    ventana.fill(Constantes.COLOR_FONDO)
 
     # Calcular movimiento jugador
     delta_x = 0
     delta_y = 0
 
     if mover_derecha == True:
-        delta_x = 5
-
+        delta_x = Constantes.VELOCIDAD_PERSONAJE
     if mover_izquierda == True:
-        delta_x = -5
-
+        delta_x = -Constantes.VELOCIDAD_PERSONAJE
     if mover_arriba == True:
-        delta_y = -5
-
+        delta_y = -Constantes.VELOCIDAD_PERSONAJE
     if mover_abajo == True:
-        delta_y = -5
+        delta_y = Constantes.VELOCIDAD_PERSONAJE
 
-        # Vamos a mover al personaje
-        jugador.mover_personaje(delta_x, delta_y)
+    jugador.movimiento(delta_x, delta_y)
 
 # Hacemos que salga el cuadrado en la ventana
     jugador.draw(ventana)
@@ -64,7 +76,16 @@ while run:
             if event.key == pygame.K_s:
                mover_abajo = True
 
-
+        # Evento para cuando soltemos la tecla dejemos de movernos
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                mover_izquierda = False
+            if event.key == pygame.K_d:
+                mover_derecha = False
+            if event.key == pygame.K_w:
+                mover_arriba = False
+            if event.key == pygame.K_s:
+                mover_abajo = False
 
 # Permite actualizar la pantalla para que se vean los cambios
     pygame.display.update()
